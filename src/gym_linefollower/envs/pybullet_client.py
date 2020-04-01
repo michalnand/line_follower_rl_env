4a2dbe5cb4505e9a06338cd1132d60b2774d1d8b
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-01 13:15:34 +0200

    common

--- src/gym_linefollower/envs/pybullet_client.py
+++ src/gym_linefollower/envs/pybullet_client.py
@@ -8,21 +8,20 @@ class PybulletClient():
 
     def __init__(self, connection_mode=pybullet.DIRECT):
         
-        time.sleep(0.5)
         self._client = pybullet.connect(connection_mode)
-        time.sleep(0.5)
         self._shapes = {}
-
-      
-
 
     def __del__(self):
         """Clean up connection if not already done."""
+        '''
         try:
             pybullet.disconnect(physicsClientId=self._client)
         except pybullet.error:
             pass
+        '''
+        pass
 
+    
     def __getattr__(self, name):
         """Inject the client id into Bullet functions."""
         attribute = getattr(pybullet, name)
