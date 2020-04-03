c338adb420b6238e6d08bfe92b3b4af96bacf65e
Michal Chovanec <michal@Michals-MacBook-Air.local>
2020-04-03 13:23:16 +0200

    common

--- example_random_agent.py
+++ example_random_agent.py
@@ -1,8 +1,8 @@
 import gym
 import gym_linefollower
 
-#env = gym.make('linefollowerRawBasic-v0') 
-env = gym.make('linefollowerRawAdvanced-v0')
+env = gym.make('linefollowerRawBasic-v0') 
+#env = gym.make('linefollowerRawAdvanced-v0')
 
 #env = gym.make('linefollowerFrames1Basic-v0') #single line, state shape 1x96x96
 #env = gym.make('linefollowerFrames4Basic-v0') #single line, state shape 4x96x96, 4 frames stacked
@@ -12,7 +12,6 @@ env = gym.make('linefollowerRawAdvanced-v0')
 #env = gym.make('linefollowerFrames1Advanced-v0') #episodic random from 32 lines, state shape 1x96x96
 #env = gym.make('linefollowerFrames4Advanced-v0') #episodic random from 32 lines, state shape 4x96x96, 4 frames stacked
 #env = gym.make('linefollowerFrames8Advanced-v0') #episodic random from 32 lines, state shape 8x96x96, 8 frames stacked
-
 
 state = env.reset()
 print("state_shape = ", state.shape)
@@ -36,4 +35,43 @@ while True:
 
     print(state)
     print("steps = ", steps, " games = ", games, " score = ", score)
+
+
+
+'''
+state = env.reset()
+print("state_shape = ", state.shape)
+
+steps = 0
+games = 0
+score = 0
     
+while True:
+
+    e0 = state[0][0]
+    e1 = state[0][1]
+    print(e0, e1)
+
+    if e0 < 0:
+        action = 1
+    else:
+        action = 0
+
+    state, reward, done, _ = env.step(action)
+
+    env.render()
+
+    steps+= 1
+    score+= reward
+
+
+  
+		
+    if done:
+        env.reset()
+        games+= 1
+
+    print(state)
+    print("steps = ", steps, " games = ", games, " score = ", score)
+
+    '''
